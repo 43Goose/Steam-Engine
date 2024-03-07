f8b8f2c660f5344b409b6a442db7c27d260aa9e4
Owen Edwards <owen.allen.edwards@gmail.com>
2024-03-07 13:10:29 -0800

    Add project files.

---
+++ Steam Engine/SteamEngine/SteamEngine.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SteamEngine.SteamEngine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+
+    public abstract class SteamEngine
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+        private string Title = "New Game";
+        private Canvas Window = null;
+        private Thread GameLoopThread = null;
+
+        public Color BackgroundColor = Color.SkyBlue;
+
+        public SteamEngine(Vector2 screenSize, string title)
+        {
+            this.ScreenSize = screenSize;
+            this.Title = title;
+
+            this.Window = new Canvas();
+            Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
+            Window.Text = this.Title;
+            Window.Paint += Renderer;
+
+            GameLoopThread = new Thread(GameLoop);
+            GameLoopThread.Start();
+
+            Application.Run(Window);
+        }
+
+        void GameLoop()
+        {
+            OnLoad();
+            while (GameLoopThread.IsAlive)
+            {
+                try
+                {
+                    OnDraw();
+                    Window.BeginInvoke((MethodInvoker)delegate { Window.Refresh(); });
+                    OnUpdate();
+                    Thread.Sleep(1);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Loading engine...");
+                }
+                
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(BackgroundColor);
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
+    }
+}
---
+++ Steam Engine/SteamEngine/Vector2.cs
@@ -0,0 +1,26 @@
+﻿namespace SteamEngine.SteamEngine
+{
+    public class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector2()
+        {
+            X = Zero().X;
+            Y = Zero().Y;
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Returns Vector 2 with X & Y as 0
+        /// </summary>
+        /// <returns>Vector2</returns>
+        public static Vector2 Zero() { return new Vector2(0, 0);}
+    }
+}
